66650d9ceb57f640dc802688e1a83f3449e3510c
and21094 <and21094@gmail.com>
2015-10-15 11:30:44 -0600

    modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/bin/Debug/Proyecto InfoAplicada.exe 	modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/bin/Debug/Proyecto InfoAplicada.pdb 	new file:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/bin/Debug/Proyecto InfoAplicada.vshost.exe.manifest 	modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/obj/Debug/Proyecto InfoAplicada.csproj.GenerateResource.Cache 	modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/obj/Debug/Proyecto InfoAplicada.exe 	modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Poryecto InfoAplicada/obj/Debug/Proyecto InfoAplicada.pdb 	modified:   InfoAplicada PROYECTO/Proyecto InfoAplicada/Proyecto InfoAplicada.v12.suo 	modified:   InfoAplicada PROYECTO/consultas.sql

--- InfoAplicada PROYECTO/consultas.sql
+++ InfoAplicada PROYECTO/consultas.sql
@@ -1,17 +1,23 @@
+--CATEGORIAS--
+select ProductCategoryID, Name from ProductCategory order by ProductCategoryID;
+
 --SUBCATEGORIAS--
-select i.CustomerID, i.FirstName, i.LastName, ps.Name, COUNT(*) as Cantidad from Individual i
+select ProductSubCategoryID, Name from ProductSubCategory order by ProductSubCategoryID; 
+
+--CLIENTES POR SUBCATEGORIAS--
+select i.CustomerID, i.FirstName, i.LastName, ps.Name as subCategoria, COUNT(*) as Cantidad from Individual i
 inner join SalesOrderHeader soh on soh.CustomerID = i.CustomerID inner join SalesOrderDetail sod
 on sod.SalesOrderID = soh.SalesOrderID
 inner join Product p on p.ProductID = sod.ProductID
 inner join ProductSubCategory ps on ps.ProductSubCategoryID = 2  
-group by i.CustomerID, i.FirstName, i.LastName, ps.Name;
+group by i.CustomerID, i.FirstName, i.LastName, ps.Name order by Cantidad desc;
 
---CATEGORIAS--
-select i.CustomerID, i.FirstName, i.LastName, count(*) AS Cantidad from Individual i
+--CLIENTES POR CATEGORIAS--
+select i.CustomerID, i.FirstName, i.LastName, pc.Name as Categoria, count(*) AS Cantidad from Individual i
 inner join SalesOrderHeader soh on soh.CustomerID = i.CustomerID inner join SalesOrderDetail sod
 on sod.SalesOrderID = soh.SalesOrderID
 inner join Product p on p.ProductID = sod.ProductID
 inner join ProductSubCategory ps on ps.ProductSubCategoryID = p.ProductSubCategoryID
 inner join ProductCategory pc on pc.ProductCategoryID = ps.ProductCategoryID and 
-pc.ProductCategoryID = 1 group by i.CustomerID, i.FirstName, i.LastName;
+pc.ProductCategoryID = 1 group by i.CustomerID, i.FirstName, i.LastName, pc.Name order by Cantidad desc;
 
